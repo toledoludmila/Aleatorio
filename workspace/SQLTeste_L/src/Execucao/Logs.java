183271dd043b9ea9c00e0be7285bd3fd0b32eb74
toledoludmila <toledo.ludmila@gmail.com>
2016-07-18 14:25:47 -0300

    novas classes

---
+++ workspace/SQLTeste_L/src/Execucao/Logs.java
@@ -0,0 +1,90 @@
+package Execucao;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Logs {
+
+	public static File arquivo;
+	public static FileWriter fr;
+	
+	public static void registrarLog(String texto) {
+		String mensagem = "[" 
+				+ new SimpleDateFormat("dd/MM/yyyy HH:mm:ss").format(new Date()) 
+				+ "] " 
+				+ texto;
+		
+		System.out.println(mensagem);
+		
+		try {
+			fr.write(mensagem + "\n");
+			fr.flush();
+		} catch (IOException e) {
+			System.out.println("Erro: " + e.getMessage());
+			e.printStackTrace();
+			System.exit(1);
+		}
+	}
+	
+	public static void registrarErro(String texto) {
+		String mensagem = "[" 
+				+ new SimpleDateFormat("dd/MM/yyyy HH:mm:ss").format(new Date()) 
+				+ "] *** ERRO *** "
+				+ texto;
+		
+		System.out.println(mensagem);
+		
+		try {
+			fr.write(mensagem + "\n");
+			fr.flush();
+		} catch (IOException e) {
+			System.out.println("Erro: " + e.getMessage());
+			e.printStackTrace();
+			System.exit(1);
+		}
+	}
+	
+	public void registrarExecucao(char inicio_fim) {
+		
+		Connection con_bdr = null;
+		try {
+			con_bdr = DriverManager.getConnection(Main.urlBDR, Main.username, Main.password);
+			Statement stm_bdr = con_bdr.createStatement();
+
+			if (inicio_fim == 'i') {
+				
+				String sqlExecI = "insert into execucao (e_idsql , data_inicio, observacao) values (" + Main.idInstrucao +",'"
+						+ new SimpleDateFormat("dd/MM/yyyy HH:mm:ss").format(new Date()).toString() + "', 'execucao com time out 90s');";
+				Main.registrarLog(sqlExecI);
+				stm_bdr.executeUpdate(sqlExecI);
+				
+				ResultSet rst_bdr = stm_bdr.executeQuery
+						("SELECT LAST_INSERT_ID() as id_execucao from execucao;");
+				rst_bdr.next();
+				Main.idExperimento = rst_bdr.getInt(1);
+				Main.registrarLog("Logs.java - registrarExecucao() - Execuçao N: "+Main.idExperimento);
+				
+			} else {
+				String sqlExecF = "update execucao set data_fim = '"
+						+ new SimpleDateFormat("dd/MM/yyyy HH:mm:ss").format(new Date()).toString()
+						+ "' where id_execucao = " + Main.idExperimento + ";";
+				stm_bdr.executeUpdate(sqlExecF);
+				Main.registrarLog(sqlExecF);
+			}
+
+			stm_bdr.close();
+			con_bdr.close();
+		} catch (SQLException e) {
+			Main.registrarErro("Logs.java - registrarExecucao() -"+ e.getMessage());
+			System.exit(1);
+		}
+	}
+}
---
+++ workspace/SQLTeste_L/src/Execucao/Main.java
@@ -0,0 +1,49 @@
+package Execucao;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import Modelo.Mutantes;
+
+public class Main extends Logs {
+	public static String urlBDP = "jdbc:mysql://localhost:3306/tpch";
+	public static String urlBDT = "jdbc:mysql://localhost:3306/tpch2";
+	public static String urlBDR = "jdbc:mysql://localhost:3306/resultados";
+	public static String urlBDM = "jdbc:mysql://localhost:3306/mutantes";
+	public static String username = "root";
+	public static String password = "senha";
+	public static String classe = "com.mysql.jdbc.Driver";
+	public static int idInstrucao = 101;
+	public static int idMutante = 0;
+	public static int idExperimento = 0;
+
+	public static void main(String[] args) {
+		Main m = new Main();
+		
+		Main.arquivo = new File("/home/ludmila/workspace/SQLTeste_L/experimento.log");
+		try {
+			Main.fr = new FileWriter(Main.arquivo);
+		} catch (IOException e) {
+			Main.registrarErro("Main.java - main() - " + e.getMessage());
+			System.exit(1);
+		}
+		m.registrarExecucao('i');
+		
+		SelecionarTuplas st = new SelecionarTuplas();
+		ArrayList<Mutantes> listaMutantes = st.buscarMutantes();
+		//ArrayList<String> listaResultado =	
+		st.obterResultado(listaMutantes);
+		//st.criarView(listaResultado);
+		
+		m.registrarExecucao('f');
+		try {
+			fr.close();
+		} catch (IOException e) {
+			Main.registrarErro("Ag.java - main() - " + e.getMessage());
+			System.exit(1);
+		}
+	}
+
+}
